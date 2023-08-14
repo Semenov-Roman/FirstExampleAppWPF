64bb43a228d47c0b5d169291e5bc05ed1ea718ca
FirstExampleWPF <pomah.2014@bk.ru>
2023-08-14 19:40:55 +0300

    first commit

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FirstExampleAppWPF.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
